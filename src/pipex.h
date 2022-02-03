ff280d9e27450630009b61b698178b7af6d2d2ef
lkrebs-l <lkrebs-l@student.42sp.org.br>
2022-02-03 04:42:30 +0100

    adding header

--- src/pipex.h
+++ src/pipex.h
@@ -6,15 +6,20 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 23:08:11 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/01/28 23:10:45 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/03 04:42:13 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <unistd.h>
+# include <stdlib.h>
 # include <sys/types.h>
+# include <unistd.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/wait.h>
 
+int	pipex(int argc, char *argv[], char *envp);
 
 # endif
