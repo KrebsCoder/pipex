4c3fa4515271a43c6ea48d2a7429432b6db3593a
KrebsCoder <lucasklopess@gmail.com>
2022-02-03 07:34:24 +0100

    update on header

--- src/pipex.h
+++ src/pipex.h
@@ -6,20 +6,33 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 23:08:11 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/03 04:42:13 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/03 07:15:08 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# include <unistd.h>
 # include <stdlib.h>
 # include <sys/types.h>
-# include <unistd.h>
 # include <sys/stat.h>
 # include <fcntl.h>
 # include <sys/wait.h>
+# include <errno.h>
+# include <stdio.h>
+
+typedef struct s_data
+{
+	int		fd[2];
+	pid_t	pid1;
+	int		pid2;
+	char	file_in;
+	char	file_out;
+}	t_data;
 
 int	pipex(int argc, char *argv[], char *envp);
+int	open_files(t_data *data, char *argv[]);
+int	close_files(t_data *data);
 
-# endif
+#endif
