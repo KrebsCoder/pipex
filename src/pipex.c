3b20a1ab63f6b4a2b2e1895524e2337300a8a278
KrebsCoder <lucasklopess@gmail.com>
2022-02-03 07:34:07 +0100

    project is starting to gain shape...

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 04:37:09 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/03 04:39:45 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/03 07:14:27 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 int	pipex(int argc, char *argv[], char *envp)
 {
-	
+	t_data data;
+
+	if (argc != 5)
+		return (EXIT_FAILURE);
+	else if (argc == 5)
+	{
+		//init_values();
+		open_files(&data, argv);
+	}
 }
