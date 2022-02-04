75e07535c9bfcfac7fae0882dccfecc880b6eb91
KrebsCoder <lucasklopess@gmail.com>
2022-02-04 21:40:32 +0100

    open_files function now properly open the fds

--- src/files.c
+++ src/files.c
@@ -6,32 +6,26 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 07:06:38 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/03 07:28:59 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/04 18:43:27 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	open_files(t_data *data, char *argv)
+int	open_files(t_data *data)
 {
-	data->file_in = open(argv[1], O_RDONLY);
-	if (data->file_in < 1)
+	data->files.in_fd = open(data->files.infile, O_RDONLY);
+	data->files.out_fd = open(data->files.outfile, O_WRONLY | O_CREAT, 0777);
+	if (data->files.in_fd < 1 || data->files.out_fd < 1)
 	{
+		//handle_error(data, 1);
 		return (EXIT_FAILURE);
-		// call error function
-	}
-		
-	data->file_out = open(argv[4], O_WRONLY | O_CREAT, 0777);
-	if (data->file_out < 1)
-	{
-		return (EXIT_FAILURE);
-		// call error function
 	}
 	return (EXIT_SUCCESS);
 }
 
 void	close_files(t_data *data)
 {
-	close(data->file_in);
-	close(data->file_out);
+	close(data->files.in_fd);
+	close(data->files.out_fd);
 }
