98705a5418d171beb594a763794d45c4647f8b05
KrebsCoder <lucasklopess@gmail.com>
2022-02-04 21:39:00 +0100

    changed the struct and added a new one

--- src/pipex.h
+++ src/pipex.h
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 23:08:11 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/04 00:47:10 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/04 21:38:00 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,27 @@
 
 typedef struct s_file
 {
-	int		fd;
-	char	*arg_v;
+	int		in_fd;
+	int		out_fd;
+	char	*infile;
+	char	*outfile;
 } t_file;
 
 typedef struct s_data
 {
 	int		fd[2];
 	pid_t	pid1;
-	int		pid2;
+	pid_t	pid2;
 	char	**envp;
 	char	*path;
 	int		cmd_qnt;
-	t_file	file_in;
-	t_file	file_out;
+	t_file files;
 }	t_data;
 
 int		pipex(int argc, char *argv[], char **envp);
-int		open_files(t_data *data, char *argv[]);
+int		open_files(t_data *data);
 void	close_files(t_data *data);
-int		exec_cmd(t_data *data, char *argv[], char **envp);
+int		exec_cmd(t_data *data, int argc, char **envp);
 void	init_values(t_data *data, int argc, char *argv[], char **envp);
 /* void	handle_error(int signal, t_data *data); */
 #endif
