a20050b3e60d299e7561797679d2797b35a3747f
KrebsCoder <lucasklopess@gmail.com>
2022-02-04 02:42:12 +0100

    changed struct

--- src/pipex.h
+++ src/pipex.h
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 23:08:11 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/03 07:15:08 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/04 00:47:10 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,28 @@
 # include <errno.h>
 # include <stdio.h>
 
+typedef struct s_file
+{
+	int		fd;
+	char	*arg_v;
+} t_file;
+
 typedef struct s_data
 {
 	int		fd[2];
 	pid_t	pid1;
 	int		pid2;
-	char	file_in;
-	char	file_out;
+	char	**envp;
+	char	*path;
+	int		cmd_qnt;
+	t_file	file_in;
+	t_file	file_out;
 }	t_data;
 
-int	pipex(int argc, char *argv[], char *envp);
-int	open_files(t_data *data, char *argv[]);
-int	close_files(t_data *data);
-
+int		pipex(int argc, char *argv[], char **envp);
+int		open_files(t_data *data, char *argv[]);
+void	close_files(t_data *data);
+int		exec_cmd(t_data *data, char *argv[], char **envp);
+void	init_values(t_data *data, int argc, char *argv[], char **envp);
+/* void	handle_error(int signal, t_data *data); */
 #endif
