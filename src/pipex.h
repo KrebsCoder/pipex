42add659a8b87ed7745be9e968e1cd51b289e5fc
KrebsCoder <lucasklopess@gmail.com>
2022-02-04 21:43:30 +0100

    function dup42 is going to handle dups

--- src/pipex.h
+++ src/pipex.h
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 23:08:11 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/04 21:38:00 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/04 21:43:03 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,6 @@ int		open_files(t_data *data);
 void	close_files(t_data *data);
 int		exec_cmd(t_data *data, int argc, char **envp);
 void	init_values(t_data *data, int argc, char *argv[], char **envp);
+void	dup42(t_data *data, int fd);
 /* void	handle_error(int signal, t_data *data); */
 #endif
