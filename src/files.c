bd60d1b5e0eaf5102c96be76d2a70ccf5e6b3e4e
KrebsCoder <lucasklopess@gmail.com>
2022-02-04 21:41:01 +0100

    small change on line 19

--- src/files.c
+++ src/files.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 07:06:38 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/04 18:43:27 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/04 21:40:46 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	open_files(t_data *data)
 {
 	data->files.in_fd = open(data->files.infile, O_RDONLY);
 	data->files.out_fd = open(data->files.outfile, O_WRONLY | O_CREAT, 0777);
-	if (data->files.in_fd < 1 || data->files.out_fd < 1)
+	if (data->files.in_fd == -1 || data->files.out_fd == -1)
 	{
 		//handle_error(data, 1);
 		return (EXIT_FAILURE);
