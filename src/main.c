d68dedeb0001c3dbfbbe44ec5a5ba2422c7830d3
KrebsCoder <lucasklopess@gmail.com>
2022-02-04 21:39:25 +0100

    same mains

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 04:38:53 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/03 06:19:40 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/03 18:00:12 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char *argv[], char *envp)
+int	main(int argc, char *argv[], char **envp)
 {
 	return (pipex(argc, argv, envp));
 }
--- src/pipex.c
+++ src/pipex.c
@@ -6,13 +6,13 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 04:37:09 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/03 07:14:27 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/04 21:38:24 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	pipex(int argc, char *argv[], char *envp)
+int	pipex(int argc, char *argv[], char **envp)
 {
 	t_data data;
 
@@ -20,7 +20,9 @@ int	pipex(int argc, char *argv[], char *envp)
 		return (EXIT_FAILURE);
 	else if (argc == 5)
 	{
-		//init_values();
-		open_files(&data, argv);
+		init_values(&data, argc, argv, envp);
+		open_files(&data);
+		exec_cmd(&data, argc, envp);
 	}
+	return (EXIT_SUCCESS);
 }
