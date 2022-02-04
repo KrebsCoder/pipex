dfd5d1b5346b9aba1e297683c9c1cc15af412c5f
KrebsCoder <lucasklopess@gmail.com>
2022-02-04 23:46:19 +0100

    added msg_error function

--- src/files.c
+++ src/files.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 07:06:38 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/04 21:40:46 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/04 23:46:02 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	open_files(t_data *data)
 	data->files.out_fd = open(data->files.outfile, O_WRONLY | O_CREAT, 0777);
 	if (data->files.in_fd == -1 || data->files.out_fd == -1)
 	{
-		//handle_error(data, 1);
+		msg_error(data);
 		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
@@ -26,6 +26,7 @@ int	open_files(t_data *data)
 
 void	close_files(t_data *data)
 {
+	close(data->fd);
 	close(data->files.in_fd);
 	close(data->files.out_fd);
 }
