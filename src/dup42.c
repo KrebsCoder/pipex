691f15cc909c7ca2bbbd7faa325b341a1b76c03f
KrebsCoder <lucasklopess@gmail.com>
2022-02-04 22:00:43 +0100

    dup42 now uses msg_error function

--- src/dup42.c
+++ src/dup42.c
@@ -6,13 +6,16 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 04:22:48 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/04 21:42:50 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/04 21:59:53 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	dup42(t_data *data, int fd)
+void	dup42(t_data *data, int fd, int std)
 {
-	
+	int check;
+	check = dup2(fd, std);
+	if (check == -1)
+		msg_error(data);
 }
