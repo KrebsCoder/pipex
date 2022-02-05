54f6bd5e3bd6a6f96aea6f3a97e95c6b8538c6f4
KrebsCoder <lucasklopess@gmail.com>
2022-02-05 03:17:13 +0100

    checkpoint

--- src/exec_cmd.c
+++ src/exec_cmd.c
@@ -6,13 +6,13 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 02:14:32 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/05 03:07:22 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/05 03:16:38 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	*exec_cmd(t_data *data, int argc, char **envp, char *argv)
+char	*path_bin(t_data *data, char *argv)
 {
 	int		i;
 	int		check;
@@ -21,12 +21,23 @@ char	*exec_cmd(t_data *data, int argc, char **envp, char *argv)
 	i = 0;
 	while (data->bin[i])
 	{
-		path = ft_strjoin(data.bin[i], '/');
+		path = ft_strjoin(data->bin[i], '/');
 		cmd = ft_strjoin(path, argv);
 		check = access(cmd, F_OK);
 		if (check == 0)
+		{
+			free(path);
 			return (cmd);
+		}
+		free(path);
+		free(cmd);
 		i++;
 	}
 	return (NULL);
+}
+
+char	exec_cmd(t_data *data, char *argv)
+{
+	char *cmd;
+	cmd = path_bin(data, argv);
 }
