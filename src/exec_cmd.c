a5137f212f450df420889573c16767e934bda8a8
KrebsCoder <lucasklopess@gmail.com>
2022-02-05 03:07:46 +0100

    exec_cmd function

--- src/exec_cmd.c
+++ src/exec_cmd.c
@@ -5,49 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/03 19:18:40 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/04 21:41:39 by lkrebs-l         ###   ########.fr       */
+/*   Created: 2022/02/05 02:14:32 by lkrebs-l          #+#    #+#             */
+/*   Updated: 2022/02/05 03:07:22 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	exec_cmd(t_data *data, int argc, char **envp)
+char	*exec_cmd(t_data *data, int argc, char **envp, char *argv)
 {
-	if (pipe(data->fd) == -1)
-		return (EXIT_FAILURE);
-	dup2(data->files.in_fd, STDIN_FILENO);
-	close(data->files.in_fd);
-	data->pid1 = fork();
- 	if (data->pid1 == -1)
+	int		i;
+	int		check;
+	char	*path;
+	char	*cmd;
+	i = 0;
+	while (data->bin[i])
 	{
-		close_files(data);
-		perror("ERROR ->");
-		return (EXIT_FAILURE);
-	} 
-	if (data->pid1 == 0)
-	{
-		dup2(data->fd[1], STDOUT_FILENO);
-		close(data->fd[0]);
-		close(data->fd[1]);
-		execlp("cat", "cat", NULL); //<<-- just for testing 
-		//execve(argv[2], &argv[1], envp);//  call function exec_cmd*
+		path = ft_strjoin(data.bin[i], '/');
+		cmd = ft_strjoin(path, argv);
+		check = access(cmd, F_OK);
+		if (check == 0)
+			return (cmd);
+		i++;
 	}
-	
-	data->pid2 = fork();
-	if (data->pid2 == 0) 
-	{ 
-		dup2(data->fd[0], STDIN_FILENO);
-		dup2(data->files.out_fd, STDOUT_FILENO);
-		close(data->fd[0]);
-		close(data->fd[1]);
-		execlp("wc", "wc", NULL);
-		//execve(argv[3], &argv[1], envp);// call function exec_cmd
- 	} 
-	return (EXIT_SUCCESS);
+	return (NULL);
 }
-
-/* int	parse_env(t_data *data, int argc, char *argv[], char **envp)
-{
-	
-} */
