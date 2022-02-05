a1518d937b4688a3a218b154b461a18cb85e6e69
KrebsCoder <lucasklopess@gmail.com>
2022-02-05 02:08:08 +0100

    parse_env function

--- src/parse_env.c
+++ src/parse_env.c
@@ -5,10 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/04 00:48:27 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/04 00:48:32 by lkrebs-l         ###   ########.fr       */
+/*   Created: 2022/02/05 00:16:40 by lkrebs-l          #+#    #+#             */
+/*   Updated: 2022/02/05 02:05:05 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void	parse_env(t_data *data, char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strnstr(envp[i], "PATH", ft_strlen("PATH")))
+		{
+			data->path = ft_strchr(envp[i], '/');
+			break ;
+		}
+		i++;
+	}
+	data->bin = ft_split(data->path, ':');
+}
