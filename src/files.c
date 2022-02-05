09ed04f5188f8c01f49cb782c872de136f5f3358
KrebsCoder <lucasklopess@gmail.com>
2022-02-05 03:09:20 +0100

    fixed close_files

--- src/files.c
+++ src/files.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 07:06:38 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/04 23:46:02 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/05 00:27:20 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 int	open_files(t_data *data)
 {
 	data->files.in_fd = open(data->files.infile, O_RDONLY);
-	data->files.out_fd = open(data->files.outfile, O_WRONLY | O_CREAT, 0777);
+	data->files.out_fd = open(data->files.outfile, O_WRONLY | O_CREAT \
+		| O_TRUNC, 0777);
 	if (data->files.in_fd == -1 || data->files.out_fd == -1)
 	{
 		msg_error(data);
@@ -26,7 +27,8 @@ int	open_files(t_data *data)
 
 void	close_files(t_data *data)
 {
-	close(data->fd);
+	close(data->fd[0]);
+	close(data->fd[1]);
 	close(data->files.in_fd);
 	close(data->files.out_fd);
 }
