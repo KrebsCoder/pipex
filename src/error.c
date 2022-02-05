b0d3b3a573fb287baf17423cac885037c8fd82aa
KrebsCoder <lucasklopess@gmail.com>
2022-02-05 02:23:47 +0100

    fixed perror message

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 17:38:12 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/04 21:58:42 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/05 00:02:33 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 void	msg_error(t_data *data)
 {
 	close_files(data);
-	perror("ERROR ->");
+	perror("ERROR");
 }
