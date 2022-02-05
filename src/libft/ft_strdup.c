899c27a3c893453f5e191ded448477d1e8c4e9dc
KrebsCoder <lucasklopess@gmail.com>
2022-02-05 01:44:49 +0100

    added ft_strdup

---
+++ src/libft/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/05 01:43:31 by lkrebs-l          #+#    #+#             */
+/*   Updated: 2022/02/05 01:43:45 by lkrebs-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../pipex.h"
+
+char	*ft_strdup(const char *src)
+{
+	int		i;
+	char	*dst;
+
+	dst = malloc(ft_strlen(src) + 1);
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
