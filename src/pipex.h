629cccbb6f8274daf3b75d8515931cb0e7d6ec2f
KrebsCoder <lucasklopess@gmail.com>
2022-02-08 01:51:41 +0100

    changed struct

--- src/pipex.h
+++ src/pipex.h
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 23:08:11 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/04 21:59:44 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/08 01:51:24 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,9 @@ typedef struct s_data
 	int		fd[2];
 	pid_t	pid1;
 	pid_t	pid2;
+	char	**cmd;
 	char	**envp;
+	char	**bin;
 	char	*path;
 	int		cmd_qnt;
 	t_file files;
@@ -44,8 +46,22 @@ typedef struct s_data
 int		pipex(int argc, char *argv[], char **envp);
 int		open_files(t_data *data);
 void	close_files(t_data *data);
-int		exec_cmd(t_data *data, int argc, char **envp);
+int		exec_cmd(t_data *data, char *argv[]);
+int		pipes(t_data *data, char **envp);
 void	init_values(t_data *data, int argc, char *argv[], char **envp);
 void	dup42(t_data *data, int fd, int std);
 void	msg_error(t_data *data);
+void	parse_env(t_data *data, char **envp);
+char	*path_bin(t_data *data);
+
+// ===== LIBFT FUNCTIONS ===== //
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_strncmp(const char *s1, const char *s2, unsigned int n);
+size_t	ft_strlen(const char *string);
+char	*ft_strchr(const char *string, int c);
+char	**ft_split(char const *s, char c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *src);
+
 #endif
