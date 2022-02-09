9f322c5e828cc70332360f34873b2221dd603d35
KrebsCoder <lucasklopess@gmail.com>
2022-02-09 20:48:09 +0100

    refactor on this function

--- src/free_all.c
+++ src/free_all.c
@@ -1,23 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free_mat.c                                         :+:      :+:    :+:   */
+/*   free_all.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 05:51:28 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/08 01:57:13 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/09 20:44:45 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-void	free_all(t_data data)
-{
-	free_mat(data.bin);
-	free_mat(data.cmd);
-	free_mat(data.envp);
-}
 
 static	void	free_mat(char **mat)
 {
@@ -31,4 +24,11 @@ static	void	free_mat(char **mat)
 	}
 	free(mat);
 	mat = NULL;
+}
+
+void	free_all(t_data *data)
+{
+	free_mat(data->bin);
+	free_mat(data->cmd);
+	exit(1);
 }
