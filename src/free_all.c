7cb05e9a2538a538f5d929fb496a0184a282d0cf
KrebsCoder <lucasklopess@gmail.com>
2022-02-10 01:25:00 +0100

    removed exit(1) from function

--- src/free_all.c
+++ src/free_all.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 05:51:28 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/09 20:44:45 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/10 01:13:30 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,4 @@ void	free_all(t_data *data)
 {
 	free_mat(data->bin);
 	free_mat(data->cmd);
-	exit(1);
 }
