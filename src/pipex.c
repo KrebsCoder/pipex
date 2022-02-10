323238f89d8e8db4587d784e9adf52a052827362
KrebsCoder <lucasklopess@gmail.com>
2022-02-10 01:49:31 +0100

    fix norm

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 04:37:09 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/02/08 03:53:39 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/02/10 01:49:00 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	pipex(int argc, char *argv[], char **envp)
 {
-	t_data data;
+	t_data	data;
 
 	if (argc != 5)
 		return (EXIT_FAILURE);
@@ -22,7 +22,7 @@ int	pipex(int argc, char *argv[], char **envp)
 	{
 		init_values(&data, argc, argv, envp);
 		open_files(&data);
-		pipes(&data, argv, envp);
+		return (pipes(&data, argv, envp));
 	}
 	return (EXIT_SUCCESS);
 }
